879925e7f92b11bcb0d45c007becb56a7778a8a9
EmanueleCuzzocrea <emanuele24c@gmail.com>
2026-07-10 09:11:13 +0000

    modified running instruction in save_realsense

--- pipeline/save_realsense_rgbd.py
+++ pipeline/save_realsense_rgbd.py
@@ -1,4 +1,4 @@
-#python save_realsense_rgbd.py --save_dir ~/shared_data --max_frames 1
+#python /home/user/PoseEstimation/pipeline/save_realsense_rgbd.py --save_dir ~/shared_data --max_frames 1
 
 import argparse
 import os
